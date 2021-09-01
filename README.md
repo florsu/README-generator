621f4d4dda1b1900662cc7bef3eded37881c4673
Christian Flores <christiangflores0824@gmail.com>
2021-08-31 20:38:12 -0700

    convert questions to object

--- README.md
+++ README.md
@@ -6,15 +6,22 @@ Title,answered
 Description,answered
 
 ## Table of Contents
+TODO
 
 ## Installation
+Installation,answered
 
 ## Usage
+Usage,answered
 
 ## License
+TODO
 
 ## Contributing
+Contributing,answered
 
 ## Tests
+TODO
 
 ## Questions
+Questions,answered
--- index.js
+++ index.js
@@ -3,72 +3,121 @@ const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+const inputtypeTextKey = 'text'
+
+
 // array of questions for user
 const questions = [
-    'Title',
-    'Description',
-    'Table of Contents',
-    'Installation',
-    'Usage',
-    'License',
-    'Contributing',
-    'Tests',
-    'Questions',
-];
+    {
+        key: 'title',
+        message: 'Title',
+        type: inputtypeTextKey,
+    },
+    {
+        key: 'description',
+        message: 'Description',
+        type: inputtypeTextKey,
+    },
+    {
+        key: 'installation',
+        message: 'Installation',
+        type: inputtypeTextKey,
+    },
+    {
+        key: 'usage',
+        message: 'Usage',
+        type: inputtypeTextKey,
+    },
+    {
+        key: 'contributing',
+        message: 'Contributing',
+        type: inputtypeTextKey,
+    },
+    {
+        key: 'questions',
+        message: 'Questions',
+        type: inputtypeTextKey,
+    },
+
+]
+/*
+[
+    
+    //'Table of Contents',
+    //'License',
+    //'Tests',
+    
+]
+*/
 
 // function to write README file
 function writeToFile(fileName, data) {
     let content = `
 ## Title
-${data.Title}
+${data.title}
 
 ## Description
-${data.Description}
+${data.description}
 
 ## Table of Contents
+TODO
 
 ## Installation
+${data.installation}
 
 ## Usage
+${data.usage}
 
 ## License
+TODO
 
 ## Contributing
+${data.contributing}
 
 ## Tests
+TODO
 
 ## Questions
+${data.questions}
 `
 
 
     fs.writeFile(fileName, content, err => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      //file written successfully
-    })  
+        if (err) {
+            console.error(err)
+            return
+        }
+        //file written successfully
+    })
 }
 
 // function to initialize program
 function init() {
+    //console.log(questions)
     let answers = {}
 
     for (let i = 0; i < questions.length; i++) {
-        let question = questions[i]
-        let answer = askQuestion(question)
+        let questionObj = questions[i]
+        // console.log(questionObj)
+        let lookup = questionObj.key
+        let answer = askQuestion(questionObj)
         console.log(`answer '${answer}'`)
         console.log('-----------------')
-        answers[question]=answer
+        answers[lookup] = answer
     }
     console.log(answers)
-    writeToFile('README.md',answers)
+    writeToFile('README.md', answers)
 }
 
 // function call to initialize program
 init();
 
-function askQuestion(question) {
-    console.log(`askQuestion '${question}'`)
+function askQuestion(questionObj) {
+    //console.log(questionObj)
+    let question = questionObj.message
+
+    //console.log(question)
+    console.log(`askQuestion '${questionObj}', '${question}'`)
+
     return `${question},answered`
 }
